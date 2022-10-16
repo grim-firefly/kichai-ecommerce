0e540d53d20bfa0af9ee663ae62a6478876468b7
Grim Firefly <khairulhasansajid@gmail.com>
2022-10-17 05:17:05 +0600

    modified topbar

--- src/Common/Header/Components/TopBar/Index.js
+++ src/Common/Header/Components/TopBar/Index.js
@@ -7,42 +7,40 @@ const TopBar = () => {
 	const [isMenuOpen, setIsMenuOpen] = useState(false);
 	const MenuRef = useRef(null);
 	const MyAccountRef = useRef(null);
-	useDropDownHooks(MenuRef,isMenuOpen, () => setIsMenuOpen(false));
-	useDropDownHooks(MyAccountRef,isMyAccount, () => setIsMyAccount(false));
+	useDropDownHooks(MenuRef, isMenuOpen, () => setIsMenuOpen(false));
+	useDropDownHooks(MyAccountRef, isMyAccount, () => setIsMyAccount(false));
 	return (
-		<>
-			<div className="flex justify-center bg-[#cccccc]">
-				<div className={`container px-4 ${s.topbar}`}>
-					<div>
-						<Link to='/'>Track Order </Link>
-						<Link to='/'>FAQ's </Link>
-						<Link to='/'>Help </Link>
-					</div>
-					<div className={`inline-flex`}>
+		<div className="flex justify-center bg-[#cccccc]">
+			<div className={`container px-1 ${s.topbar}`}>
+				<div>
+					<Link className='hover:underline mx-1 ' to='/'>Track Order </Link>
+					<Link className='hover:underline mx-1' to='/'>FAQ's </Link>
+					<Link className='hover:underline mx-1' to='/'>Help </Link>
+				</div>
+				<div className={`inline-flex`}>
 
-						<div className='relative' ref={MyAccountRef}>
-							<div><button className='hover:underline mx-1' onClick={() => setIsMyAccount(!isMyAccount)}>MyAccount</button></div>
-							{isMyAccount &&
-								<div className={`${s.dropdown}`} >
-									<button className='text-left hover:underline' onClick={() => { console.log("hi"); }}> LogIn </button>
-									<button className='text-left hover:underline'> Register </button>
-								</div>
-							}
-						</div>
-						<div className='relative' ref={MenuRef}>
-							<div><button className='hover:underline mx-1' onClick={() => setIsMenuOpen(!isMenuOpen)}>Language</button></div>
-							{isMenuOpen &&
-								<div className={`${s.dropdown}`} >
-									<button className='text-left hover:underline'> EN </button>
-									<button className='text-left hover:underline'> BA </button>
-									<button className='text-left hover:underline'> US </button>
-								</div>
-							}
-						</div>
+					<div className='relative' ref={MyAccountRef}>
+						<div><button className='hover:underline mx-1' onClick={() => setIsMyAccount(!isMyAccount)}>MyAccount</button></div>
+						{isMyAccount &&
+							<div className={`${s.dropdown}`} >
+								<button className='text-left hover:underline' onClick={() => { console.log("hi"); }}> LogIn </button>
+								<button className='text-left hover:underline'> Register </button>
+							</div>
+						}
+					</div>
+					<div className='relative' ref={MenuRef}>
+						<div><button className='hover:underline mx-1' onClick={() => setIsMenuOpen(!isMenuOpen)}>Language</button></div>
+						{isMenuOpen &&
+							<div className={`${s.dropdown}`} >
+								<button className='text-left hover:underline'> EN </button>
+								<button className='text-left hover:underline'> BA </button>
+								<button className='text-left hover:underline'> US </button>
+							</div>
+						}
 					</div>
 				</div>
 			</div>
-		</>
+		</div>
 	);
 }
 export default TopBar;
